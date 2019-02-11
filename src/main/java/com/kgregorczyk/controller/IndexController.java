9ed4a1be495d3f6ed20d762023e0a41aaed512b1
Kamil Gregorczyk <gregorczyk@me.com>
2019-02-11 20:37:30 +0100

    Adjusts k8 configs to be compatible with a replica set

--- src/main/java/com/kgregorczyk/controller/IndexController.java
+++ src/main/java/com/kgregorczyk/controller/IndexController.java
@@ -1,5 +1,15 @@
 package com.kgregorczyk.controller;
 
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+
+@Controller()
 public class IndexController {
 
+  @Get
+  @Secured("isAnonymous()")
+  public String index() {
+    return "Hello World!";
+  }
 }
